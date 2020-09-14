36976233756e3e71227e29cdece819fa51405533
sixstagesofpain <dimitrievegor@gmail.com>
2020-09-14 18:40:02 +0300

    fixed lesson 1

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -120,7 +120,7 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
 fun accountInThreeYears(initial: Int, percent: Int): Double =
-    initial * (1.0 + percent / 100.00) * (1.0 + percent / 100.00) * (1.0 + percent / 100.00)
+    initial * (1.0 + percent / 100.00).pow(3)
 
 /**
  * Простая (2 балла)
