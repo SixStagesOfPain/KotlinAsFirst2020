edc378b97b26fc0ba3c432b580fc9557b0e72496
sixstagesofpain <dimitrievegor@gmail.com>
2020-09-17 02:01:29 +0300

    lesson 3 completed + some tasks from lesson 4

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -133,7 +133,10 @@ fun abs(v: List<Double>): Double {
  *
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
-fun mean(list: List<Double>): Double = TODO()
+fun mean(list: List<Double>): Double {
+    return if (list.isEmpty()) 0.0
+    else list.sum() / list.size
+}
 
 /**
  * Средняя (3 балла)
