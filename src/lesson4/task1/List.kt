424177d13580b5d4f745149e2adc8ab2d12a23a6
sixstagesofpain <dimitrievegor@gmail.com>
2020-09-18 04:18:41 +0300

    some tasks from lesson 4

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -347,7 +347,27 @@ fun decimal(digits: List<Int>, base: Int): Int {
  * Использовать функции стандартной библиотеки, напрямую и полностью решающие данную задачу
  * (например, str.toInt(base)), запрещается.
  */
-fun decimalFromString(str: String, base: Int): Int = TODO()
+fun rebase(d: Char): Int {
+    return when (d) {
+        'a' -> 10; 'b' -> 11; 'c' -> 12; 'd' -> 13
+        'e' -> 14; 'f' -> 15; 'g' -> 16; 'h' -> 17
+        'i' -> 18; 'j' -> 19; 'k' -> 20; 'l' -> 21
+        'm' -> 22; 'n' -> 23; 'o' -> 24; 'p' -> 25
+        'q' -> 26; 'r' -> 27; 's' -> 28; 't' -> 29
+        'u' -> 30; 'v' -> 31; 'w' -> 32; 'x' -> 33
+        'y' -> 34; 'z' -> 35; '1' -> 1; '2' -> 2
+        '3' -> 3; '4' -> 4; '5' -> 5; '6' -> 6
+        '7' -> 7; '8' -> 8; '9' -> 9; '0' -> 0
+        else -> 0
+    }
+}
+
+fun decimalFromString(str: String, base: Int): Int {
+    var res = 0
+    for (i in str.indices)
+        res += rebase(str[i]) * base.toDouble().pow(str.length - i - 1).toInt()
+    return res
+}
 
 /**
  * Сложная (5 баллов)
