3a178e8154a65419f06f44363fb2e891b60bdc2e
sixstagesofpain <dimitrievegor@gmail.com>
2020-09-18 03:55:36 +0300

    some tasks from lesson 4

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -328,7 +328,12 @@ fun convertToString(n: Int, base: Int): String {
  * из системы счисления с основанием base в десятичную.
  * Например: digits = (1, 3, 12), base = 14 -> 250
  */
-fun decimal(digits: List<Int>, base: Int): Int = TODO()
+fun decimal(digits: List<Int>, base: Int): Int {
+    var res = 0
+    for (i in digits.indices)
+        res += digits[i] * base.toDouble().pow(digits.size - i - 1).toInt()
+    return res
+}
 
 /**
  * Сложная (4 балла)
